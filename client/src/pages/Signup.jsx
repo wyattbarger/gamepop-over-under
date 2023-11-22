405f1d97f5aaad301119d516956ffd4850063c8c
Wyatt Barger <wyattbarger@icloud.com>
2023-11-21 22:07:57 -0600

    Added padding to the left and right side of CardTitle to better match the style currently present in the homepage, recently pushed by Alex Blaylock.

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 // Add the necessary technologies and components for the Signup page.
-import React from 'react';
-import styled from '@emotion/styled';
+import React from "react";
+import styled from "@emotion/styled";
 
 // Add the styled components for the Signup page.
 const Container = styled.div`
@@ -15,15 +15,17 @@ const SignupCard = styled.div`
   align-items: center;
   width: 90%;
   max-width: 1000px;
-  border: 1px solid black;
+  border: 1px solid #008f11;
   border-radius: 10px;
   margin: 10px;
-  padding: 25px;
+  padding: 20px;
 `;
 
 const CardTitle = styled.h1`
-  font-size: 100px;
+  color: #008f11;
+  font-size: 8rem;
   white-space: nowrap;
+  padding: 0 20px;
 `;
 
 const SignupForm = styled.form`
@@ -33,38 +35,42 @@ const SignupForm = styled.form`
 `;
 
 const UsernameLabel = styled.label`
+  color: #008f11;
   font-size: 16px;
   white-space: nowrap;
 `;
 
 const UsernameInput = styled.input`
+  color: #008f11;
   font-size: 20px;
 `;
 
 const PasswordLabel = styled.label`
+  color: #008f11;
   font-size: 16px;
   white-space: nowrap;
 `;
 
 const PasswordInput = styled.input`
+  color: #008f11;
   font-size: 20px;
 `;
 
 // Add the function which builds out our Signup page with styled components.
 function Signup() {
-    return (
-        <Container>
-            <SignupCard>
-                <CardTitle>Join Game Pop</CardTitle>
-                <SignupForm>
-                    <UsernameLabel htmlFor="username">Username</UsernameLabel>
-                    <UsernameInput type="text" id="username" />
-                    <PasswordLabel htmlFor="password" >Password</PasswordLabel>
-                    <PasswordInput type="password" id="password" />
-                </SignupForm>
-            </SignupCard>
-        </Container>
-    )
-};
+  return (
+    <Container>
+      <SignupCard>
+        <CardTitle>Join Game Pop</CardTitle>
+        <SignupForm>
+          <UsernameLabel htmlFor="username">Choose a Username</UsernameLabel>
+          <UsernameInput type="text" id="username" />
+          <PasswordLabel htmlFor="password">Enter a Password</PasswordLabel>
+          <PasswordInput type="password" id="password" />
+        </SignupForm>
+      </SignupCard>
+    </Container>
+  );
+}
 
 export default Signup;
