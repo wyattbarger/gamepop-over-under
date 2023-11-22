ae616fe82de942c0768183e12b65a612f1ffe950
Wyatt Barger <wyattbarger@icloud.com>
2023-11-21 18:58:44 -0600

    Commit current changes to merge pull request from Main.

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+//Add necessary technologies for the Footer component.
+import React from 'react'; 
+
+// Add the Footer component.
+function Footer() {
+    return (
+        <section>
+
+        </section>
+    )
+};
