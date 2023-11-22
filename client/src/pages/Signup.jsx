b97c364f423c7e68f8c8b4e0934b051020e159c9
Wyatt Barger <wyattbarger@icloud.com>
2023-11-21 21:50:19 -0600

    Added basic signup card to the Signup.jsx page using styled components.

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,20 +1,69 @@
 // Add the necessary technologies and components for the Signup page.
 import React from 'react';
-
 import styled from '@emotion/styled';
 
-const FooterContainer = styled.footer`
-    background-color: #0D0D0D;
-    padding: 10px;
-      
-`
+// Add the styled components for the Signup page.
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
-// Add the component which builds out our Signup page.
+const SignupCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 90%;
+  max-width: 1000px;
+  border: 1px solid black;
+  border-radius: 10px;
+  margin: 10px;
+  padding: 25px;
+`;
+
+const CardTitle = styled.h1`
+  font-size: 100px;
+  white-space: nowrap;
+`;
+
+const SignupForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const UsernameLabel = styled.label`
+  font-size: 16px;
+  white-space: nowrap;
+`;
+
+const UsernameInput = styled.input`
+  font-size: 20px;
+`;
+
+const PasswordLabel = styled.label`
+  font-size: 16px;
+  white-space: nowrap;
+`;
+
+const PasswordInput = styled.input`
+  font-size: 20px;
+`;
+
+// Add the function which builds out our Signup page with styled components.
 function Signup() {
     return (
-        <FooterContainer>
-            
-        </FooterContainer>
+        <Container>
+            <SignupCard>
+                <CardTitle>Join Game Pop</CardTitle>
+                <SignupForm>
+                    <UsernameLabel htmlFor="username">Username</UsernameLabel>
+                    <UsernameInput type="text" id="username" />
+                    <PasswordLabel htmlFor="password" >Password</PasswordLabel>
+                    <PasswordInput type="password" id="password" />
+                </SignupForm>
+            </SignupCard>
+        </Container>
     )
 };
 
