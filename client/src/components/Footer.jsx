0a30c70221150b93c72a3f6c9ce7a20d494bd69d
Wyatt Barger <wyattbarger@icloud.com>
2023-11-21 19:19:59 -0600

    Commit to pull routing changes from Andrew.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,16 @@
 //Add necessary technologies for the Footer component.
 import React from 'react'; 
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 
 // Add the Footer component.
 function Footer() {
     return (
         <section>
+            <Container>
 
+            </Container>
         </section>
     )
 };
