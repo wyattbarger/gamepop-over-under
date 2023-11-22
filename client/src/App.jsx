90c85e09773a91aa22cc9b40936d6c198d3f4b8e
ahmell <andrewmell10@gmail.com>
2023-11-21 19:16:42 -0600

    routing is functional

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,30 @@
-import { useState } from 'react'
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './pages/home.jsx';
+import { useState } from "react";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { Outlet } from "react-router-dom";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Home from "./pages/home.jsx";
 
 const client = new ApolloClient({
-  uri: '/graphql',
-  cache: new InMemoryCache()
+  uri: "/graphql",
+  cache: new InMemoryCache(),
 });
 
-
 // page import
-import StartMenu from './pages/StartMenu';
-import PlayGame from './pages/PlayGame';
-import Login from './pages/Login';
-import EndMenu from './pages/EndMenu';
+import StartMenu from "./pages/StartMenu";
+import PlayGame from "./pages/PlayGame";
+import Login from "./pages/Login";
+import EndMenu from "./pages/EndMenu";
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Home />
-      </ApolloProvider>
-  )
+      <div className="flex-column justify-center align-center min-100-vh bg-primary">
+        <Outlet />
+      </div>
+    </ApolloProvider>
+  );
 }
 
-export default App
+export default App;
