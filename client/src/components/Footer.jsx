7aff9f511f48080c98f056c7964fa7059c1df1d8
Wyatt Barger <wyattbarger@icloud.com>
2023-11-24 13:55:51 -0600

    Added the Copyright component to the Footer with the mvp styling I had a rough idea for to get an idea into code. Next steps will be to bring in a Github logo linking to the repo, as well as icon to represent issues or bugs, which will link to the issues tab of the repo. These will display at the 'end' of the footer with appropriate padding added.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,34 +3,48 @@ import React, { useState } from "react";
 import styled from "@emotion/styled";
 
 // Add the styled components for the Footer component.
-// Add the FooterContainer which handles the parent styling of the footer icon elements.   
+// Add the FooterContainer which handles the parent styling of the footer icon elements.
 const FooterContainer = styled.footer`
-border-top: 1px solid #1a1a1a;
-display: flex;
-justify-content: space-between;
-align-items: center;
-position: fixed;
-bottom: 0;
-left: 0;
-width: 100%;
-padding: 10px;
-border-top: 1px solid #000;
-background-color: #f5f5f5;
+  border-top: 1px solid #444;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  padding: 10px;
+  background-color: #0d0d0d;
+  transition: border-top-color 0.7s ease, color 0.7s ease, box-shadow 0.7s ease;
+
+  &:hover {
+    border-top-color: #005f0c;
+    color: #005f0c;
+    box-shadow: 0 0 15px #005f0c;
+  }
 `;
 
 // Add a Copyright styled component for the respective line in the 'Footer' exported component, add margin: 0 to remove default browser <p> styling.
 const Copyright = styled.p`
   margin: 0;
   font-size: 12px;
-  color: #1a1a1a;
-`;
+  color: #444;
+  transition: color 0.7s ease;
 
+  &:hover {
+    color: #005f0c;
+  }
+`;
 
 // Add the Footer component as the default export for the file.
 export default function Footer() {
-    return (
-        <FooterContainer>
-            <Copyright>© 2023 Wyatt Barger, Andrew Mell, and Alex Blaylock. This project is licensed under the MIT License. Designed and developed in Novemeber 2023. </Copyright>
-        </FooterContainer>
-    );
-};
+  return (
+    <FooterContainer>
+      <Copyright>
+        © 2023 Wyatt Barger, Andrew Mell, and Alex Blaylock. This project is
+        licensed under the MIT License. Designed and developed in Novemeber
+        2023.
+      </Copyright>
+    </FooterContainer>
+  );
+}
