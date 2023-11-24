b838106a91f774d779facd757727f4c58a7749f9
Wyatt Barger <wyattbarger@icloud.com>
2023-11-24 17:30:50 -0600

    Added icons and got positioning correct for them in the footer component.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,8 @@
 // Add the necessary technologies and for the Footer component.
 import React, { useState } from "react";
 import styled from "@emotion/styled";
+import githubIcon from '../assets/github-icon-png.png';
+import issuesIcon from '../assets/issues-icon-png.png';
 
 // Add the styled components for the Footer component.
 // Add the FooterContainer which handles the parent styling of the footer icon elements.
@@ -13,14 +15,14 @@ const FooterContainer = styled.footer`
   bottom: 0;
   left: 0;
   width: 100%;
-  padding: 10px;
+    padding: 10px 40px 10px 0; 
   background-color: #0d0d0d;
   transition: border-top-color 0.7s ease, color 0.7s ease, box-shadow 0.7s ease;
 
   &:hover {
     border-top-color: #005f0c;
     color: #005f0c;
-    box-shadow: 0 0 15px #005f0c;
+    box-shadow: 0 0 7px #005f0c;
   }
 `;
 
@@ -36,6 +38,36 @@ const Copyright = styled.p`
   }
 `;
 
+// Add a FooterIcon styled component for the Github and Issues icons that will display at the 'end' of the footer.
+const GithubIcon = styled.img`
+  width: 24px;
+  height: 24px;
+  transition: filter 0.7s ease;
+  margin-right: 10px;
+
+  &:hover {
+    filter: drop-shadow(0 0 3px #005f0c);
+  }
+`;
+
+// Add a FooterIcon styled component for the Github and Issues icons that will display at the 'end' of the footer.
+const IssuesIcon = styled.img`
+  width: 24px;
+  height: 24px;
+  transition: filter 0.7s ease;
+
+  &:hover {
+    filter: drop-shadow(0 0 3px #005f0c);
+  }
+`;
+
+// Add a IconContainer styled component for the Github and Issues icons that will display at the 'end' of the footer.
+const IconContainer = styled.div`
+    display: flex;
+    align-items: center;
+    padding-right: 15px;
+`;
+
 // Add the Footer component as the default export for the file.
 export default function Footer() {
   return (
@@ -45,6 +77,10 @@ export default function Footer() {
         licensed under the MIT License. Designed and developed in Novemeber
         2023.
       </Copyright>
+      <IconContainer>
+        <GithubIcon src={githubIcon} />
+        <IssuesIcon src={issuesIcon} />
+      </IconContainer>
     </FooterContainer>
   );
 }
