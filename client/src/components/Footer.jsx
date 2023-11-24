46370870d1b18a820a3793ffa4f920c5a8bc55a0
Wyatt Barger <wyattbarger@icloud.com>
2023-11-24 13:29:42 -0600

    Slight alterations to styled components to meet the mvp design. Added a Copyright component as a styled p element which will be used to list the copyright info and developer names in the left of the footer.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,24 +3,34 @@ import React, { useState } from "react";
 import styled from "@emotion/styled";
 
 // Add the styled components for the Footer component.
+// Add the FooterContainer which handles the parent styling of the footer icon elements.   
 const FooterContainer = styled.footer`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: fixed;
-  bottom: 0;
-  left: 50%;
-  transform: translateX(-50%);
-  padding: 10px;
-  border: 1px solid #000;
-  border-radius: 5px;
-  background-color: #f5f5f5;
+border-top: 1px solid #1a1a1a;
+display: flex;
+justify-content: space-between;
+align-items: center;
+position: fixed;
+bottom: 0;
+left: 0;
+width: 100%;
+padding: 10px;
+border-top: 1px solid #000;
+background-color: #f5f5f5;
 `;
+
+// Add a Copyright styled component for the respective line in the 'Footer' exported component, add margin: 0 to remove default browser <p> styling.
+const Copyright = styled.p`
+  margin: 0;
+  font-size: 12px;
+  color: #1a1a1a;
+`;
+
 
 // Add the Footer component as the default export for the file.
 export default function Footer() {
     return (
         <FooterContainer>
+            <Copyright>© 2023 Wyatt Barger, Andrew Mell, and Alex Blaylock. This project is licensed under the MIT License. Designed and developed in Novemeber 2023. </Copyright>
         </FooterContainer>
     );
 };
