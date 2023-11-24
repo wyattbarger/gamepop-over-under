fea0471166b9d704781466c17bd759fbe69313a0
Wyatt Barger <wyattbarger@icloud.com>
2023-11-24 12:48:04 -0600

    Added the base structure of the footer mvp I will be making and testing with Signup.jsx.

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+// Add the necessary technologies and for the Footer component.
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+
+// Add the styled components for the Footer component.
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+// Add the Footer component as the default export for the file.
+export default function Footer() {
+
+};
