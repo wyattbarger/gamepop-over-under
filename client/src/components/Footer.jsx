2082bbac91204cd0945dfb22bcfd56435f6547ed
Wyatt Barger <wyattbarger@icloud.com>
2023-11-24 12:56:31 -0600

    Added the FooterContainer as a styled footer component, and added the intial styling to test to meet the goal of a mvp.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,13 +3,24 @@ import React, { useState } from "react";
 import styled from "@emotion/styled";
 
 // Add the styled components for the Footer component.
-const Container = styled.div`
+const FooterContainer = styled.footer`
   display: flex;
-  flex-direction: row;
+  justify-content: center;
   align-items: center;
+  position: fixed;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  padding: 10px;
+  border: 1px solid #000;
+  border-radius: 5px;
+  background-color: #f5f5f5;
 `;
 
 // Add the Footer component as the default export for the file.
 export default function Footer() {
-
+    return (
+        <FooterContainer>
+        </FooterContainer>
+    );
 };
