f206a8e6475666cc204c6551ba99fcec672d70cf
Wyatt Barger <wyattbarger@icloud.com>
2023-11-25 00:49:06 -0600

    Added more styling to the Header component to try to reach a mvp. Need to import and add small icons, and then add a effect to change the tab color on hover, and make sure that it stay that color when selected, to have a decent looking mvp.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,8 @@
 // Add the technologies necessary to make the Header component functional.
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import React, { useState } from "react";
 import styled from "@emotion/styled";
- // * Need to add the auth information that will need to be created for the front end to work with the back end, which should be created in the utils directory to decode the JWT token *.
+// * Need to add the auth information that will need to be created for the front end to work with the back end, which should be created in the utils directory to decode the JWT token *.
 
 // Add the styled components for the Header component.
 const HeaderContainer = styled.header`
@@ -10,40 +10,70 @@ const HeaderContainer = styled.header`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  padding: 20px;
+  height: 100px;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding: 10px 10px 10px 10px;
   margin-bottom: 20px;
+  border-bottom: 1px solid #008f11;
 `;
 
 const HeroContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding-left: 10px;
 `;
 
 const HeaderLogo = styled.img``;
 
-const HeaderTitle = styled.h1``;
+const HeaderTitle = styled.h1`
+  color: #008f11;
+`;
 
 const Navbar = styled.nav`
   display: flex;
+  align-items: flex-start;
   justify-content: space-evenly;
+  padding: 10px 40px 10px 10px;
 `;
 
-const HeaderLink = styled(Link)``;
+const NavbarLink = styled(Link)`
+  text-decoration: none;
+  color: #008f11;
+  padding: 10px 40px 10px 10px;
+
+  &:after {
+    content: "";
+    position: relative;
+    bottom: 0;
+    left: 0;
+    width: 0;
+    height: 1px;
+    background: #008f11;
+    transition: width 0.3s ease-in-out;
+  }
+
+  &:hover:after {
+    width: auto;
+  }
+`;
 
 export default function Header() {
-    return (
-        <HeaderContainer>
-            <HeroContainer>
-                <HeaderLogo src="" alt="logo" />
-                <HeaderTitle>React Trivia</HeaderTitle>
-            </HeroContainer>
-            <Navbar>
-                <HeaderLink href="/">Home</HeaderLink>
-                <HeaderLink href="/play">Play</HeaderLink>
-                <HeaderLink href="/login">Login</HeaderLink>
-                <HeaderLink href="/signup">Signup</HeaderLink>
-            </Navbar>
-        </HeaderContainer>
-    );
-};
+  return (
+    <HeaderContainer>
+      <HeroContainer>
+        <HeaderLogo src="" alt="logo" />
+        <HeaderTitle>Game Pop</HeaderTitle>
+      </HeroContainer>
+      <Navbar>
+        <NavbarLink to="/">Home</NavbarLink>
+        <NavbarLink to="/play">Play</NavbarLink>
+        <NavbarLink to="/login">Login</NavbarLink>
+        <NavbarLink to="/signup">Signup</NavbarLink>
+      </Navbar>
+    </HeaderContainer>
+  );
+}
