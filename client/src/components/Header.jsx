e64ecd937fc37451808ad943f250b16977a61b1a
Wyatt Barger <wyattbarger@icloud.com>
2023-11-26 02:11:07 -0600

    Added useLocation from react to define a location variable and a currentPath variable to style the header links conditionally with the active hook from react-router-dom. Formatted Header.jsx with prettier before push.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,7 @@
 // Add the technologies necessary to make the Header component functional.
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import React, { useState } from "react";
 import styled from "@emotion/styled";
-import headerLogo from "../assets/mvp-icon-png.png";
 // * Need to add the auth information that will need to be created for the front end to work with the back end, which should be created in the utils directory to decode the JWT token *.
 
 // Add the styled components for the Header component.
@@ -27,7 +26,7 @@ const HeroContainer = styled.div`
   padding: 10px 10px 10px 10px;
 `;
 const HeaderTitle = styled.h1`
-  font-size: 50.5px;
+  font-size: 38.5px;
   color: #008f11;
   text-shadow: 3px 4px 4px rgba(0, 255, 65, 1);
 `;
@@ -36,29 +35,47 @@ const Navbar = styled.nav`
   display: flex;
   align-items: flex-start;
   justify-content: space-evenly;
-  padding: 5px 5px 5px 5px;
   border-top: 1px solid #008f11;
   border-bottom: 1px solid #008f11;
   border-left: 1px solid #008f11;
 `;
 
+// Add the active prop to the NavbarLink that will style according to the active route in react-router-dom.
 const NavbarLink = styled(Link)`
   text-decoration: none;
-  color: #008f11;
+  color: ${(props) => (props.active ? "#0d0d0d" : "#008f11")};
+  background-color: ${(props) => (props.active ? "#008f11" : "transparent")};
   padding: 10px 40px 10px 10px;
+  transition: color 0.3s ease-in-out, background-color 0.6s ease-in-out;
+
+  &:hover {
+    color: #0d0d0d;
+    background-color: #008f11;
+  }
 `;
 
+// Add the Header component as the default export for the file.
 export default function Header() {
+  const location = useLocation();
+  const currentPath = location.pathname;
   return (
     <HeaderContainer>
       <HeroContainer>
         <HeaderTitle>Game Pop</HeaderTitle>
       </HeroContainer>
       <Navbar>
-        <NavbarLink to="/">Home</NavbarLink>
-        <NavbarLink to="/play">Play</NavbarLink>
-        <NavbarLink to="/login">Login</NavbarLink>
-        <NavbarLink to="/signup">Signup</NavbarLink>
+        <NavbarLink to="/" active={currentPath === "/"}>
+          Home
+        </NavbarLink>
+        <NavbarLink to="/play" active={currentPath === "/play"}>
+          Play
+        </NavbarLink>
+        <NavbarLink to="/login" active={currentPath === "/login"}>
+          Login
+        </NavbarLink>
+        <NavbarLink to="/signup" active={currentPath === "/signup"}>
+          Signup
+        </NavbarLink>
       </Navbar>
     </HeaderContainer>
   );
